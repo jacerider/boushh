ddf9470ba3b70ca7e7dbaa5fb3a2732d85b9e6da
callinmullaney <callin@callinmullaney.me>
2015-03-03 08:54:50 -0600

    added body background texture. fixed js bug to set min-height and not height. adjusted vertical tab styling

--- assets/js/boushh.js
+++ assets/js/boushh.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.verticalTabsHeight = {
     attach: function(context, settings) {
       var highestCol = Math.max($('.vertical-tabs-list').height(),$('.vertical-tabs-list').height());
-	  jQuery('.vertical-tabs-panes').height(highestCol);
+	  jQuery('.vertical-tabs-panes').css("min-height",highestCol);
     }
   };
 
