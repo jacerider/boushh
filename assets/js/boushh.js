3f26dbfef2384a1b24e09018fb09e359421e6fe2
JaceRider <jacerider@gmail.com>
2015-04-29 14:54:08 -0500

    Style updates.

--- assets/js/boushh.js
+++ assets/js/boushh.js
@@ -2,6 +2,10 @@
 
 Drupal.behaviors.boushh = {
   attach: function(context, settings) {
+    $('.form-item, .form-wrapper').addClass(function() {
+      var $parent = $(this.parentNode).closest('fieldset, .fieldset-wrapper, .form-item, .form-wrapper');
+      return $parent.hasClass('boushh-inset') ? 'boushh-outset' : 'boushh-inset';
+    });
   }
 };
 
